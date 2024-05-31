a4a90936ae399711edbe5e27791feb9c71416166
GERARDOHDZHTA <gerardohdzhta@gmail.com>
2024-05-31 17:00:01 -0600

    Update streamlit_app.py
    Add new lines at bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,13 @@ if ingredients_list:
     
     
     time_to_insert = st.button('Submmit Order')
-    
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
 
-    st.stop()
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
+st.stop()
